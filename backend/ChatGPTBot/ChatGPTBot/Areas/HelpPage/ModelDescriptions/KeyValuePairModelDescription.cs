1bbb690fdab85d8c0b033ab8b677868753629f15
shivji71 <shivji7158@gmail.com>
2025-09-27 16:10:11 +0530

    Remove backend submodule pointer

--- backend/ChatGPTBot/ChatGPTBot/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatGPTBot.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
